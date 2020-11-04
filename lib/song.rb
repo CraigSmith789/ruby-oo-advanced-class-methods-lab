6ac8cf71d481b2e60aabe8983584109ea08fa749
CraigSmith789 <craig789@gmail.com>
2020-11-04 17:10:22 -0500

    commit tree 032dcf028af526ab5faa7be49298babd9f46e59d

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,13 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def self.create
+    s = self.new
+    s.save
+    s    
+  end
 
   def self.all
     @@all
@@ -10,4 +17,58 @@ class Song
     self.class.all << self
   end
 
-end
+  def self.new_by_name(string)
+    s = self.new
+    s.name = string
+    s
+  end
+  def self.create_by_name(string)
+    s = self.new
+    s.name = string
+    @@all<<s 
+    s
+  end
+  def self.find_by_name(string)
+    @@all.detect{|s| s.name == string}
+  end
+  def self.find_or_create_by_name(string)
+      dupe_check = self.find_by_name(string)
+      if dupe_check == nil
+        self.create_by_name(string)
+      else
+        dupe_check 
+      end
+  end
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}
+
+  end
+  def self.new_from_filename(file)
+    infod = file.slice!".mp3" #-----removes mp3
+    info = file.split(/\s-\ /) #---removes - 
+    song_name = info[1]
+    artist_name = info[0]
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+
+  end
+  def self.destroy_all
+    @@all.clear
+
+  end
+  def self.create_from_filename(file)
+    infod = file.slice!".mp3"
+    info = file.split(/\s-\ /)
+    song_name = info[1]
+    artist_name = info[0]
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    @@all<<song
+    song
+
+  end
+  
+end 
